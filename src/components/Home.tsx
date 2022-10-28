9e1aecc5ca8e9c9e88369aa12848d93ca6e4a93e
niamhbrockbank <niamh.brockbank@gmail.com>
2022-10-28 18:05:22 +0100

    Format with prettier

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ import Results from "./Results";
 export default function Home(): JSX.Element {
   const [location, setLocation] = useState<ILocation>({
     latitude: "52.729100005889684",
-    longitude: "-1.0915778066014774"
+    longitude: "-1.0915778066014774",
   });
 
   return (
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -32,30 +32,34 @@ export default function Results({ location }: IProps): JSX.Element {
   const sunriseFormatted = formatTime(sunrise);
   const sunsetFormatted = formatTime(sunset);
 
-  const sunTimeRemaing = moment(sunsetFormatted, "HH:mm").fromNow()
+  const sunTimeRemaing = moment(sunsetFormatted, "HH:mm").fromNow();
 
   return (
     <>
-    <div id='sun_remaining'>
-      {sunTimeRemaing.includes("ago") ? 
-        <p>Sunset was <mark>{sunTimeRemaing}</mark>.</p>
-        :
-        <p>You have <mark>{sunTimeRemaing.slice(2)}</mark> of sunlight left.</p>
-      }
-    </div>
-    <div className="results">
-      <div className="sun_time">
-        <img className="sun_image" src="./sunrise.png" alt="sunrise" />
-        <h1 className="time">{sunriseFormatted}</h1>
-        <p className="time_description">sunrise</p>
+      <div id="sun_remaining">
+        {sunTimeRemaing.includes("ago") ? (
+          <p>
+            Sunset was <mark>{sunTimeRemaing}</mark>.
+          </p>
+        ) : (
+          <p>
+            You have <mark>{sunTimeRemaing.slice(2)}</mark> of sunlight left.
+          </p>
+        )}
       </div>
+      <div className="results">
+        <div className="sun_time">
+          <img className="sun_image" src="./sunrise.png" alt="sunrise" />
+          <h1 className="time">{sunriseFormatted}</h1>
+          <p className="time_description">sunrise</p>
+        </div>
 
-      <div className="sun_time">
-        <img className="sun_image" src="./sunset.png" alt="sunset" />
-        <h1 className="time">{sunsetFormatted}</h1>
-        <p className="time_description">sunset</p>
+        <div className="sun_time">
+          <img className="sun_image" src="./sunset.png" alt="sunset" />
+          <h1 className="time">{sunsetFormatted}</h1>
+          <p className="time_description">sunset</p>
+        </div>
       </div>
-    </div>
     </>
   );
 }
