96332dc411189d14c03f0b6b17d5c3bb3f9b94f0
niamhbrockbank <niamh.brockbank@gmail.com>
2022-10-29 14:02:02 +0100

    Improve daylight saving formatting

--- src/utils/formatTime.test.ts
+++ src/utils/formatTime.test.ts
@@ -5,12 +5,14 @@ test("afternoon hours are converted right", () => {
   expect(formatTime("2:46:55 PM")).toBe(moment().isDST() ? "15:46" : "14:46");
   expect(formatTime("10:46:55 PM")).toBe(moment().isDST() ? "23:46" : "22:46");
   expect(formatTime("12:46:55 PM")).toBe(moment().isDST() ? "13:46" : "12:46");
-  expect(formatTime("11:46:55 PM")).toBe(moment().isDST() ? "12:46" : "11:46");
+  expect(formatTime("11:46:55 PM")).toBe(moment().isDST() ? "00:46" : "23:46");
 });
 
 test("all instances have four time digits", () => {
   expect(formatTime("8:55:43 AM")).toBe(moment().isDST() ? "09:55" : "08:55");
-  expect(formatTime("9:55:43 AM")).toBe(moment().isDST() ? "10:55" : "09:55");
+  expect(formatTime("9:36:43 AM")).toBe(moment().isDST() ? "10:36" : "09:36");
+  expect(formatTime("11:55:43 AM")).toBe(moment().isDST() ? "12:55" : "11:55");
+  expect(formatTime("12:55:43 AM")).toBe(moment().isDST() ? "01:55" : "00:55");
 });
 
 test.todo("adjust for different time zones");
--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -6,15 +6,30 @@ export default function formatTime(time: string): string {
   const arr = time.split(":");
   arr[0] = `${parseInt(arr[0]) + daylightSaving}`;
 
+  //Morning UTC
   if (time.slice(-2) === "AM") {
+    //Convert 12AM to 24hr 
+    if (!daylightSaving && arr[0] === '12'){
+      return `00:${arr[1]}`
+    } else if (daylightSaving && arr[0] === '13'){
+      return `01:${arr[1]}`
+    }
+
+    //Before 10 morning UTC add padding
     if (parseInt(arr[0]) < 10) {
       return `0${arr[0]}:${arr[1]}`;
     } else {
       return `${arr[0]}:${arr[1]}`;
     }
+
   } else {
-    if ((daylightSaving === 1 && arr[0] === "13") || arr[0] === "12") {
-      return `${arr[0]}:${arr[1]}`;
+    //All UTC PM
+    if (!daylightSaving && arr[0] === "12"){
+      return `12:${arr[1]}`;
+    } else if (daylightSaving && arr[0] === "13"){
+      return `13:${arr[1]}`
+    } else if (daylightSaving && arr[0] === "12"){
+      return `00:${arr[1]}`
     } else {
       const twentyFourHour = parseInt(arr[0]) + 12;
       return `${twentyFourHour}:${arr[1]}`;
