5c11deeea318de3f78b593085c1285a35874a34b
niamhbrockbank <niamh.brockbank@gmail.com>
2022-10-29 13:29:57 +0100

    Account for daylight savings

--- src/utils/formatTime.test.ts
+++ src/utils/formatTime.test.ts
@@ -1,12 +1,16 @@
+import moment from "moment";
 import formatTime from "./formatTime";
 
 test("afternoon hours are converted right", () => {
-  expect(formatTime("2:46:55 PM")).toBe("14:46");
-  expect(formatTime("10:46:55 PM")).toBe("22:46");
+  expect(formatTime("2:46:55 PM")).toBe(moment().isDST() ? "15:46" : "14:46");
+  expect(formatTime("10:46:55 PM")).toBe(moment().isDST() ? "23:46" : "22:46");
+  expect(formatTime("12:46:55 PM")).toBe(moment().isDST() ? "13:46" : "12:46");
+  expect(formatTime("11:46:55 PM")).toBe(moment().isDST() ? "12:46" : "11:46");
 });
 
 test("all instances have four time digits", () => {
-  expect(formatTime("8:55:43 AM")).toBe("08:55");
+  expect(formatTime("8:55:43 AM")).toBe(moment().isDST() ? "09:55" : "08:55");
+  expect(formatTime("9:55:43 AM")).toBe(moment().isDST() ? "10:55" : "09:55");
 });
 
 test.todo("adjust for different time zones");
--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,14 +1,26 @@
+import moment from "moment";
+
 export default function formatTime(time: string): string {
+  const daylightSaving = moment().isDST() ? 1 : 0;
+
+  const arr = time.split(":");
+  arr[0] = `${parseInt(arr[0]) + daylightSaving}`;
+
   if (time.slice(-2) === "AM") {
-    const arr = time.split(":");
     if (parseInt(arr[0]) < 10) {
-      return `0${time.slice(0, 4)}`;
+      return `0${arr[0]}:${arr[1]}`;
     } else {
-      return time.slice(0, 5);
+      return `${arr[0]}:${arr[1]}`;
     }
   } else {
-    const arr = time.split(":");
-    const twentyFourHour = parseInt(arr[0]) + 12;
-    return `${twentyFourHour}:${arr[1]}`;
+    if (
+      (daylightSaving && arr[0] === "13") ||
+      (daylightSaving && arr[0] === "12")
+    ) {
+      return `${arr[0]}:${arr[1]}`;
+    } else {
+      const twentyFourHour = parseInt(arr[0]) + 12;
+      return `${twentyFourHour}:${arr[1]}`;
+    }
   }
 }
