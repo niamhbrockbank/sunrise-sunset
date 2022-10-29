ff177f274b15bdba4bb985a6f867f0c562851088
niamhbrockbank <niamh.brockbank@gmail.com>
2022-10-29 14:03:23 +0100

    Format with prettier

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -8,11 +8,11 @@ export default function formatTime(time: string): string {
 
   //Morning UTC
   if (time.slice(-2) === "AM") {
-    //Convert 12AM to 24hr 
-    if (!daylightSaving && arr[0] === '12'){
-      return `00:${arr[1]}`
-    } else if (daylightSaving && arr[0] === '13'){
-      return `01:${arr[1]}`
+    //Convert 12AM to 24hr
+    if (!daylightSaving && arr[0] === "12") {
+      return `00:${arr[1]}`;
+    } else if (daylightSaving && arr[0] === "13") {
+      return `01:${arr[1]}`;
     }
 
     //Before 10 morning UTC add padding
@@ -21,15 +21,14 @@ export default function formatTime(time: string): string {
     } else {
       return `${arr[0]}:${arr[1]}`;
     }
-
   } else {
     //All UTC PM
-    if (!daylightSaving && arr[0] === "12"){
+    if (!daylightSaving && arr[0] === "12") {
       return `12:${arr[1]}`;
-    } else if (daylightSaving && arr[0] === "13"){
-      return `13:${arr[1]}`
-    } else if (daylightSaving && arr[0] === "12"){
-      return `00:${arr[1]}`
+    } else if (daylightSaving && arr[0] === "13") {
+      return `13:${arr[1]}`;
+    } else if (daylightSaving && arr[0] === "12") {
+      return `00:${arr[1]}`;
     } else {
       const twentyFourHour = parseInt(arr[0]) + 12;
       return `${twentyFourHour}:${arr[1]}`;
